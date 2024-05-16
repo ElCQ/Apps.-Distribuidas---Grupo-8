645976a58b6f247857e395eb59e446c23e2b71dd
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-16 20:09:32 -0300

    created GenreController class

---
+++ src/controllers/genreController.js
@@ -0,0 +1,24 @@
+import genreService from '../services/genreService.js';
+import logger from '../utils/logger.js';
+
+let instance = null;
+
+class GenreController{
+    getAllGenres = async (req, res, next) => {
+        try{
+            let items = await movieService.getAllItems();
+            logger.info(`GET REQUEST successful for all genres`);
+            res.status(200).json(items);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    static getInstance(){
+        if(!instance){
+            instance = new GenreController();
+        }
+        return instance;
+    }
+}
+export default GenreController.getInstance();
