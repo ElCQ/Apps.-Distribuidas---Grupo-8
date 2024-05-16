c16527d3f825b1474a8b2d2a8ac21a428012bfdc
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-16 19:59:40 -0300

    created movieController

---
+++ src/controllers/movieController.js
@@ -0,0 +1,65 @@
+import movieService from '../movies/movieService.js';
+import logger from '../utils/logger.js';
+
+let instance = null;
+
+class MovieController{
+    getAllMovies = async (req, res, next) => {
+        try{
+            let items = await movieService.getAllItems();
+            logger.info(`GET REQUEST successful for all movies`);
+            res.status(200).json(items);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    getMovieByID = async (req, res, next) => {
+        try{
+            let item = await movieService.getMovie(req.params.id);
+            logger.info(`GET REQUEST successful for movie ${req.params.id}`);
+            res.status(200).json(item);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    getMovieCommentsByID = async (req, res, next) => {
+        try{
+            let item = await movieService.getMovieComments(req.params.id);
+            logger.info(`GET REQUEST successful for movie ${req.params.id}`);
+            res.status(200).json(item);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    getMovieContentByID = async (req, res, next) => {
+        try{
+            let item = await movieService.getMovieContent(req.params.id);
+            logger.info(`GET REQUEST successful for movie ${req.params.id}`);
+            res.status(200).json(item);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    postMovie = async (req, res, next) => {
+        try{
+            let movieID = await movieService.createMovie(req.body.name, req.body.price, req.body.image, req.body.description, req.body.categories, 
+                                                               req.body.responsible, req.body.duration, req.body.frequency, req.body.comments, req.body.qualification, req.body.published, req.body.type);
+            logger.info(`POST REQUEST successful for movie ${movieID}`);
+            res.status(200).json({message: `The movie with ID ${movieID} was added to the catalog.`});
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    static getInstance(){
+        if(!instance){
+            instance = new MovieController();
+        }
+        return instance;
+    }
+}
+export default MovieController.getInstance();
