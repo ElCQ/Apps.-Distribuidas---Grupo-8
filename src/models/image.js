1242c1cc81d73a96e5e7d82e64be3cd28d3f2e9e
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-17 11:44:59 -0300

    created Image Model

---
+++ src/models/image.js
@@ -0,0 +1,60 @@
+import imageToBase64 from "image-to-base64"
+class Image{
+    #id
+    #aspect_ratio
+    #height
+    #width
+    #file_path
+    constructor({aspect_ratio, height, width, file_path, id}){
+        this.#aspect_ratio = aspect_ratio;
+        this.#height = height;
+        this.#width = width;
+        this.#file_path = file_path;
+        this.#id = id;
+    }
+    getAspectRatio(){
+        return this.#aspect_ratio;
+    }
+    setAspectRatio(aspect_ratio){
+        this.#aspect_ratio = aspect_ratio;
+    }
+    getHeight(){
+        return this.#height;
+    }
+    setHeight(height){
+        this.#height = height;
+    }
+    getWidth(){
+        return this.#width;
+    }
+    setWidth(width){
+        this.#width = width;
+    }
+    getFilePath(){
+        return this.#file_path;
+    }
+    setFilePath(file_path){
+        this.#file_path = file_path;
+    }
+    getID(){
+        return this.#id;
+    }
+    setID(id){
+        this.#id = id;
+    }
+    toDTO(){
+        const dto = {
+            aspect_ratio: this.#aspect_ratio,
+            height: this.#height,
+            width: this.#width,
+            file_path: this.#file_path,
+            id: this.#id
+        }
+        return dto
+    }
+    async toBase64(){
+        const imageUrl = 'https://image.tmdb.org/t/p/original/'+this.#file_path
+        return await imageToBase64(imageUrl)
+    }
+}
+export default Image;
