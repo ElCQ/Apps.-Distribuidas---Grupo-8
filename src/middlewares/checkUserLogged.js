a14a23580f8f4f5964aab2169494cdef26b9873e
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-17 13:38:13 -0300

    implemented JWT validation in middleware

--- src/middlewares/checkUserLogged.js
+++ src/middlewares/checkUserLogged.js
@@ -1,10 +1,13 @@
 import { Error } from "../error/error.js";
+import userService from "../services/userService.js";
 
 export default function checkUserLogged(req, res, next) {
-    next();//TODO remove when implementation of JWT is done
     if(!req.header.authorization){
         throw new Error('You need to be logged in to perform this action', 'FORBIDDEN')
     }
-    //TODO check JWT...
+    let session = userService.validateJWT(req.header.authorization);
+    if(!session){
+        throw new Error('You need to be logged in to perform this action', 'FORBIDDEN')
+    }
     next();
 }
