8d9a195188fe458b04d37261ff784c6041b05d34
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-16 21:20:13 -0300

    refactored errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,32 +1,31 @@
 import logger from "../utils/logger.js";
 
 export default function errorHandler(err, req, res, next){
+    logger.error(err.message)
     switch (err.type){
-        case 'NOT_FOUND':{
-            res.status(404);
-            break;
-        }
-        case 'CONFLICT':{
-            res.status(409);
-            break;
-        }
-        case 'FORBIDDEN':{
-            res.status(403);
-            break;
-        }
         case 'BAD_REQUEST':{
-            res.status(400);
+            res.status(400).json({statusCode: 400, message: err.message});;
             break;
         }
         case 'UNAUTHORIZED':{
-            res.status(401);
+            res.status(401).json({statusCode: 401, message: err.message});;
+            break;
+        }
+        case 'FORBIDDEN':{
+            res.status(403).json({statusCode: 403, message: err.message});;
+            break;
+        }
+        case 'NOT_FOUND':{
+            res.status(404).json({statusCode: 404, message: err.message});
+            break;
+        }
+        case 'CONFLICT':{
+            res.status(409).json({statusCode: 409, message: err.message});;
             break;
         }
         default: {
-            res.status(500);
+            res.status(500).json({statusCode: 500, message: err.message});;
             break;
         }
     }
-    logger.error(err.message)
-    res.json({message: err.message})
 }
