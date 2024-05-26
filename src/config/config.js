902f0b2d497eeaf630df2041d351ec118aa50bd9
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-05-25 23:32:34 -0300

    Changed structure of config

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
-
-export default {
+const config = {
     PORT: process.PORT,
     SECRET_KEY: process.SECRET_KEY,
     GOOGLE_SIGN_IN_CLIENT_ID: process.GOOGLE_SIGN_IN_CLIENT_ID,
     SESSION_EXPIRY_TIME: process.SESSION_EXPIRY_TIME,
     CNX_STRING: process.CNX_STRING,
     DB_NAME: process.DB_NAME
-};
+}
+export default config;
