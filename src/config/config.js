52f2b71fbf7635f1a16acb182faedf1022c01808
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-06-02 19:50:50 -0300

    added missing configuration

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,10 @@ const config = {
     GOOGLE_SIGN_IN_CLIENT_ID: process.env.GOOGLE_SIGN_IN_CLIENT_ID,
     SESSION_EXPIRY_TIME: +process.env.SESSION_EXPIRY_TIME,
     CNX_STRING: process.env.CNX_STRING,
-    DB_NAME: process.env.DB_NAME
+    DB_NAME: process.env.DB_NAME,
+    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+    CLOUDINARY_API: process.env.CLOUDINARY_API,
+    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET
 }
+
 export default config;
