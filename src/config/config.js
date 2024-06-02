5095a11abc22ec0681565501b561cf03996dbe65
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-06-02 20:46:56 -0300

    removed unused configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 const config = {
     PORT: +process.env.PORT,
     SECRET_KEY: process.env.SECRET_KEY,
-    GOOGLE_SIGN_IN_CLIENT_ID: process.env.GOOGLE_SIGN_IN_CLIENT_ID,
     SESSION_EXPIRY_TIME: +process.env.SESSION_EXPIRY_TIME,
     CNX_STRING: process.env.CNX_STRING,
     DB_NAME: process.env.DB_NAME,
