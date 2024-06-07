7c5cdd50cf07eed0fe9dc9181e865932435f2a85
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-06-07 18:50:06 -0300

    fixed validation

--- src/validations/userDataValidation.js
+++ src/validations/userDataValidation.js
@@ -1,23 +1,14 @@
 import { Error } from "../error/error.js";
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export default function userDataValidation({email, nickname, firstname, lastname, image}) {
-    if(nickname === ""){
+export default function userDataValidation({nickname, firstname, lastname}) {
+    if(nickname === ""  || nickname === undefined){
         throw new Error(`The user nickname can not be empty.`, 'BAD_REQUEST')
     }
-    if(email === ""){
-        throw new Error(`The user email can not be empty.`, 'BAD_REQUEST')
-    }
-    if(re.test(email)){
-        throw new Error(`The user email can not be empty.`, 'BAD_REQUEST')
-    }
-    if(firstname === ""){
+    if(firstname === ""  || firstname === undefined){
         throw new Error(`The user firstname can not be empty.`, 'BAD_REQUEST')
     }
-    if(lastname === ""){
+    if(lastname === ""  || lastname === undefined){
         throw new Error(`The user lastname can not be empty.`, 'BAD_REQUEST')
-    }
-    if(image === ""){
-        throw new Error(`The user image can not be empty.`, 'BAD_REQUEST')
     }
 }
